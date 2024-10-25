c14838aa1dc64cdc0aee79ba7b02214960b06cfa
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-10-24 22:08:49 -0700

    export library from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,40 @@
-export function sum(a: number, b: number): number {
-  return a + b;
-}
+import { types } from "util";
+import { Agent } from "./Agent";
+import { ASGI } from "./ASGI";
+import { Communication } from "./Communication";
+import { Config } from "./Config";
+// import { Context } from "./Context"
+import { Dispatch } from "./Dispatch";
+import { Envelope } from "./Envelope";
+import { mailbox } from "./Mailbox";
+import { model } from "./model";
+import { Protocol } from "./Protocol";
+import { query } from "./Query";
+import { Registration } from "./Registration";
+import { Storage } from "./Storage";
+import { Wallet } from "./Wallet";
+
+const uAgents = {
+  Agent,
+  ASGI,
+  Communication,
+  Config,
+  // Context,
+  crypto,
+  Dispatch,
+  Envelope,
+  mailbox,
+  model,
+  // Network,
+  Protocol,
+  query,
+  Registration,
+  // Resolver,
+  // setup,
+  Storage,
+  types,
+  // utils,
+  Wallet,
+};
+
+export default uAgents;
