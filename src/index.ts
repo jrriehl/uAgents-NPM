62f34e090299f05e30b48e2de02e834ef9624b51
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-11-13 18:26:07 -0800

    export all from index

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,15 @@
-import { types } from "util";
-import { Agent } from "./Agent";
-import { ASGI } from "./ASGI";
-import { Communication } from "./Communication";
-import { Config } from "./Config";
-// import { Context } from "./Context"
-import { Dispatch } from "./Dispatch";
-import { Envelope } from "./Envelope";
-import { mailbox } from "./Mailbox";
-import { model } from "./model";
-import { Protocol } from "./Protocol";
-import { query } from "./Query";
-import { Registration } from "./Registration";
-import { Storage } from "./Storage";
-import { Wallet } from "./Wallet";
-
-const uAgents = {
-  Agent,
-  ASGI,
-  Communication,
-  Config,
-  // Context,
-  crypto,
-  Dispatch,
-  Envelope,
-  mailbox,
-  model,
-  // Network,
-  Protocol,
-  query,
-  Registration,
-  // Resolver,
-  // setup,
-  Storage,
-  types,
-  // utils,
-  Wallet,
-};
-
-export default uAgents;
+export * from "util";
+export * from "./Agent";
+export * from "./ASGI";
+export * from "./Communication";
+export * from "./Config";
+export * from "./Context";
+export * from "./Dispatch";
+export * from "./Envelope";
+export * from "./Mailbox";
+export * from "./model";
+export * from "./Protocol";
+export * from "./Query";
+export * from "./Registration";
+export * from "./Storage";
+export * from "./Wallet";
